5cd6afc3ec98503ed86fb2e39b0d5adce8c21258
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2023-03-22 08:06:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const unsplashURL = 'https://source.unsplash.com/random/'
+const unsplashURL = 'https://source.unsplash.com/random/';
+
+const container = document.querySelector('.container');
+
+const linhas = 10;
+
+for(let i = 0; i < linhas * 3; i++){
+
+    const img = document.createElement('img');
+    img.src = `${unsplashURL}${getRandomTamanho()}`;
+    container.appendChild(img);
+}
+
+function getRandomTamanho(){
+    return `${getRandomTamanho}x${getRandom()}`
+}
+
+function getRandom(){
+    return Math.floor(Math.random() * 10 + 300)
+}
